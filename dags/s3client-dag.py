8b695e981042906507b8cb2c001e1be1cba6fb2c
ssulca <ser.0090@gmail.com>
2021-11-03 18:12:40 -0300

    fix

--- dags/s3client-dag.py
+++ dags/s3client-dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.providers.amazon.aws.operators.s3_bucket import S3CopyObjectOperator
+# from airflow.providers.amazon.aws.operators.s3_bucket import S3CopyObjectOperator
 from airflow.contrib.operators.s3_list_operator import S3ListOperator
 from airflow.version import version
 from datetime import datetime, timedelta
